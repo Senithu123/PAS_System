f5457e1613a9b937ef67452dec775692a0a93b6a
Isala-123 <mahanamaisala@gmail.com>
2026-04-19 13:45:03 +0530

    created the login part

---
+++ PAS.Web/Views/Account/RegisterStudent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PAS.Web.Views.Account
+{
+    public class RegisterStudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
